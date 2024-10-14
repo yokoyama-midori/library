a9e73f4d9bf69afce27781678fe8b46684636de5
yokoyama-midori <nittundoraemon@gmail.com>
2024-10-14 21:39:49 +0900

    a

---
+++ test/library_checker/data_structure/point_add_range_sum.test.cpp
@@ -0,0 +1,31 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/point_add_range_sum"
+#include "data_structure/segtree.hpp"
+#include "template.hpp"
+
+ll e() {
+    return 0;
+}
+ll op(ll a, ll b) {
+    return a + b;
+}
+void solve() {
+    LL(n, q);
+    vl a(n);
+    input(a);
+    segtree<ll, op, e> seg(a);
+    rep(_, q) {
+        LL(flag);
+        if(flag == 0) {
+            LL(p, x);
+            seg.set(p, seg.get(p) + x);
+        } else {
+            LL(l, r);
+            print(seg.prod(l, r));
+        }
+    }
+}
+int main() {
+    ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    solve();
+}
