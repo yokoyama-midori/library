37faff31436a898e68dd3176941e0c239b1ee376
yokoyama-midori <nittundoraemon@gmail.com>
2024-10-16 15:35:40 +0900

    a

---
+++ poly/bostan_mori.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#include "template.hpp"
+#include <atcoder/modint>
+template <class T> T bostanMori(vector<T> p, vector<T> q, ll n) {
+    // return [x^n]P(x)/Q(x)
+    ll k = p.size();
+    while(n) {
+        auto q_minus(q);
+        rep(i, k + 1) {
+            if(i & 1)
+                q_minus[i] *= -1;
+        }
+        if(T::mod() == 998244353) {
+            p = convolution(p, q_minus);
+            q = convolution(q, q_minus);
+        } else {
+            p = convolution_naive(p, q_minus);
+            q = convolution_naive(q, q_minus);
+        }
+        vector<T> q_nex(k + 1), p_nex(k);
+        rep(i, k + 1) { q_nex[i] = q[2 * i]; }
+        ll n1 = n & 1;
+        rep(i, k) { p_nex[i] = p[2 * i + n1]; }
+        swap(p, p_nex);
+        swap(q, q_nex);
+        n /= 2;
+    }
+    return p[0] / q[0];
+}
--- test/atcoder/tdpc_fibonacci.test.cpp
+++ test/atcoder/tdpc_fibonacci.test.cpp
@@ -1,27 +1,25 @@
 #define PROBLEM "https://atcoder.jp/contests/tdpc/tasks/tdpc_fibonacci"
 // 行列累乗では無理 (高速)きたまさ法,Bostan-Moriが必要
-int main(){}
-// #include "template.hpp"
-// #include <atcoder/modint>
-// using namespace atcoder;
-// using mint = modint1000000007;
-// #include "math/matrix.hpp"
-// void solve() {
-//     LL(k, n);
-//     if(n <= k) {
-//         print(1);
-//         return;
-//     }
-//     Matrix<mint> A(k);
-//     rep(i, k) A[0][i] = 1;
-//     rep(i, 1, k) A[i][i - 1] = 1;
-//     A ^= (n - k);
-//     print(accumulate(all(A[0]), mint(0)).val());
-// }
-// int main() {
-//     ios::sync_with_stdio(false);
-//     std::cin.tie(nullptr);
-//     cout << std::setprecision(16);
-//     int t = 1;
-//     rep(_, t) { solve(); }
-// }
+#include "template.hpp"
+#include "poly/bostan_mori.hpp"
+#include <atcoder/convolution>
+#include <atcoder/modint>
+using namespace atcoder;
+using mint = modint1000000007;
+void solve() {
+    LL(k, n);
+    vector<mint> f(k), q(k + 1);
+    rep(i, k) f[i] = 1;
+    q[0] = 1;
+    rep1(i, k) q[i] = -1;
+    auto p = convolution_naive(f,q);
+    p.resize(k);
+    print(bostanMori(p,q,n-1).val());
+}
+int main() {
+    ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    cout << std::setprecision(16);
+    int t = 1;
+    rep(_, t) { solve(); }
+}
