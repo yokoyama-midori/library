3c9f7ff2a014f0d16c16c7e44ee5c62b5fcadfcd
yokoyama-midori <nittundoraemon@gmail.com>
2024-10-19 02:25:42 +0900

    a

--- test/atcoder/tdpc_fibonacci.test.cpp
+++ test/atcoder/tdpc_fibonacci.test.cpp
@@ -1,5 +1,5 @@
-#define PROBLEM "https://atcoder.jp/contests/tdpc/tasks/tdpc_fibonacci"
 #define IGNORE
+#define PROBLEM "https://atcoder.jp/contests/tdpc/tasks/tdpc_fibonacci"
 // 行列累乗では無理 (高速)きたまさ法,Bostan-Moriが必要
 #include "template.hpp"
 #include "poly/bostan_mori.hpp"
