8f9afbbc693f2034f84ec0834033890505da29f8
yokoyama-midori <nittundoraemon@gmail.com>
2024-10-24 15:50:51 +0900

    a

--- template.hpp
+++ template.hpp
@@ -8,24 +8,32 @@ using namespace std;
 // https://xn--kst.jp/blog/2019/08/29/cpp-comp/
 // debug methods
 // usage: debug(x,y);
+// vector 出力できるように修正
+template <typename T>
+ostream& debug_print(ostream& os, const vector<T>& v) {
+    os << "[";
+    for (size_t i = 0; i < v.size(); ++i) {
+        os << v[i];
+        if (i < v.size() - 1) os << ", ";
+    }
+    os << "]";
+    return os;
+}
+template <typename T>
+ostream& debug_print(ostream& os, const T& var) {
+    os << var;
+    return os;
+}
 #define CHOOSE(a) CHOOSE2 a
 #define CHOOSE2(a0, a1, a2, a3, a4, x, ...) x
-#define debug_1(x1) cout << #x1 << ": " << x1 << endl
-#define debug_2(x1, x2)                                                        \
-    cout << #x1 << ": " << x1 << ", " #x2 << ": " << x2 << endl
-#define debug_3(x1, x2, x3)                                                    \
-    cout << #x1 << ": " << x1 << ", " #x2 << ": " << x2 << ", " #x3 << ": "    \
-         << x3 << endl
-#define debug_4(x1, x2, x3, x4)                                                \
-    cout << #x1 << ": " << x1 << ", " #x2 << ": " << x2 << ", " #x3 << ": "    \
-         << x3 << ", " #x4 << ": " << x4 << endl
-#define debug_5(x1, x2, x3, x4, x5)                                            \
-    cout << #x1 << ": " << x1 << ", " #x2 << ": " << x2 << ", " #x3 << ": "    \
-         << x3 << ", " #x4 << ": " << x4 << ", " #x5 << ": " << x5 << endl
+#define debug_1(x1) { cout << #x1 << ": "; debug_print(cout, x1) << endl; }
+#define debug_2(x1, x2) { cout << #x1 << ": "; debug_print(cout, x1) << ", " << #x2 << ": "; debug_print(cout, x2) << endl; }
+#define debug_3(x1, x2, x3) { cout << #x1 << ": "; debug_print(cout, x1) << ", " << #x2 << ": "; debug_print(cout, x2) << ", " << #x3 << ": "; debug_print(cout, x3) << endl; }
+#define debug_4(x1, x2, x3, x4) { cout << #x1 << ": "; debug_print(cout, x1) << ", " << #x2 << ": "; debug_print(cout, x2) << ", " << #x3 << ": "; debug_print(cout, x3) << ", " << #x4 << ": "; debug_print(cout, x4) << endl; }
+#define debug_5(x1, x2, x3, x4, x5) { cout << #x1 << ": "; debug_print(cout, x1) << ", " << #x2 << ": "; debug_print(cout, x2) << ", " << #x3 << ": "; debug_print(cout, x3) << ", " << #x4 << ": "; debug_print(cout, x4) << ", " << #x5 << ": "; debug_print(cout, x5) << endl; }
+
 #ifdef LOCAL
-#define debug(...)                                                             \
-    CHOOSE((__VA_ARGS__, debug_5, debug_4, debug_3, debug_2, debug_1, ~))      \
-    (__VA_ARGS__)
+#define debug(...) CHOOSE((__VA_ARGS__, debug_5, debug_4, debug_3, debug_2, debug_1, ~))(__VA_ARGS__)
 #else
 #define debug(...)
 #endif
