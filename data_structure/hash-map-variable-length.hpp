2b88b52cc32453cf25e3c0b0047a39163bd351db
yokoyama-midori <nittundoraemon@gmail.com>
2024-11-11 06:01:42 +0900

    a

---
+++ data_structure/hash-map-variable-length.hpp
@@ -0,0 +1,79 @@
+#pragma once
+#include "template.hpp"
+template <class Val> struct HashMap {
+    int sz, mask, cnt;
+    vector<bool> used;
+    vector<ll> keys;
+    vector<Val> vals;
+    HashMap(int n = 1) {
+        sz = 1;
+        while(sz < 2 * n) {
+            sz <<= 1;
+        }
+        mask = sz - 1;
+        cnt = 0;
+        keys.resize(sz);
+        vals.resize(sz);
+        used.resize(sz);
+    }
+
+    Val &operator[](const ll &key) {
+        if(cnt * 2 > sz) {
+            reallocate();
+        }
+        ll i = index(key);
+        if(!used[i]) {
+            used[i] = true;
+            keys[i] = key;
+            cnt++;
+        }
+        return vals[i];
+    }
+    bool contains(const ll &key) {
+        ll i = index(key);
+        return used[i];
+    }
+
+  private:
+    int hash(ll x) {
+        // https://judge.yosupo.jp/submission/186759
+        static const ll r =
+            std::chrono::steady_clock::now().time_since_epoch().count();
+        x += r;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return (x ^ (x >> 31)) & mask;
+    }
+    int index(const ll &key) {
+        ll i = hash(key);
+        while(used[i] and keys[i] != key) {
+            i = (i + 1) & mask;
+        }
+        return i;
+    }
+    void reallocate() {
+        ll old_sz = sz;
+        sz <<= 1;
+        mask = sz - 1;
+        cnt = 0;
+
+        vector<ll> old_keys;
+        vector<Val> old_vals;
+        vector<bool> old_used;
+        keys.swap(old_keys);
+        vals.swap(old_vals);
+        used.swap(old_used);
+        keys.resize(sz);
+        vals.resize(sz);
+        used.resize(sz);
+
+        rep(i, old_sz) {
+            if(old_used[i]) {
+                ll key = old_keys[i];
+                Val val = old_vals[i];
+                (*this)[key] = val;
+            }
+        }
+        return;
+    }
+};
---
+++ test/library_checker/data_structure/associative_array.test.cpp
@@ -0,0 +1,22 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/associative_array"
+#include "data_structure/hash-map-variable-length.hpp"
+#include "template.hpp"
+void solve() {
+    LL(q);
+    HashMap<ll> a(q);
+    // or HashMap<ll> a;
+    rep(_, q) {
+        LL(flag, k);
+        if(flag == 0) {
+            LL(v);
+            a[k] = v;
+        } else {
+            print(a[k]);
+        }
+    }
+}
+int main() {
+    ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    solve();
+}
