3d3d85377cfd31da5d3a80734d81db8dbaea6472
yokoyama-midori <nittundoraemon@gmail.com>
2024-12-02 00:25:59 +0900

    a

---
+++ geometry/manhattan-mst.hpp
@@ -0,0 +1,43 @@
+#include "template.hpp"
+// Hai Zhou, Narendra Shenoy, and William Nicholls. 2001. Efficient minimum
+// spanning tree construction without Delaunay triangulation.
+template <class T = long long>
+vector<tuple<T, int, int>> manhattan_mst(vector<T> &xs, vector<T> &ys) {
+    // O(N)個の最小全域木の辺の候補{(重み,i,j)}
+    assert(ssize(xs) == ssize(ys));
+    vector<tuple<T, int, int>> res;
+    int n = ssize(xs);
+    res.reserve(2 * n);
+    vector<int> ord(n);
+    iota(all(ord), 0);
+    auto cul = [&]() -> void {
+        map<T, int, greater<T>> mp;
+        for(auto i : ord) {
+            for(auto itr = mp.lower_bound(xs[i]); itr != end(mp);
+                itr = mp.erase(itr)) {
+                int j = itr->second;
+                if(xs[j] - ys[j] < xs[i] - ys[i]) {
+                    break;
+                }
+                T dist = (ys[i] - ys[j]) + (xs[i] - xs[j]);
+                res.emplace_back(dist, i, j);
+            }
+            mp[xs[i]] = i;
+        }
+    };
+    rep(cnt, 4) {
+        if(cnt & 1) {
+            swap(xs, ys);
+        } else {
+            if(cnt == 2) {
+                for(auto &&x : xs)
+                    x *= -1;
+            }
+            sort(all(ord),
+                 [&](int i, int j) { return xs[i] + ys[i] < xs[j] + ys[j]; });
+        }
+        cul();
+    }
+    sort(all(res));
+    return res;
+}
---
+++ test/library_checker/geometry/manhattanmst.test.cpp
@@ -0,0 +1,29 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/manhattanmst"
+#include "geometry/manhattan-mst.hpp"
+#include <atcoder/dsu>
+void solve() {
+    LL(n);
+    vl xs(n), ys(n);
+    rep(i, n) cin >> xs[i] >> ys[i];
+    const auto &dxy = manhattan_mst(xs, ys);
+    atcoder::dsu g(n);
+    ll w = 0;
+    vector<P> edge;
+    edge.reserve(n - 1);
+    for(auto &[d, i, j] : dxy) {
+        if(!g.same(i, j)) {
+            g.merge(i, j);
+            w += d;
+            edge.emplace_back(i, j);
+        }
+    }
+    print(w);
+    for(auto [i, j] : edge) {
+        print(i, j);
+    }
+}
+int main() {
+    ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    solve();
+}
