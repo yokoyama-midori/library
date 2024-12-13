b63b9d64a99310e26778e3fdc0083e31996f3af8
yokoyama-midori <nittundoraemon@gmail.com>
2024-12-13 16:21:28 +0900

    beatsの記事に合わせてmodify
    これはではvf[x]がxを含むxの部分木に対する作用だったが、vf[x]をxより真に下側の要素に対する作用となるよう変更した
    https://yukicoder.me/submissions/470340 からACLの実装に近づけた

--- data_structure/lazy_segtree.hpp
+++ data_structure/lazy_segtree.hpp
@@ -2,8 +2,8 @@
 
 template <class S, S (*op)(S, S), S (*e)(), class F, S (*mapping)(F, S),
           F (*composition)(F, F), F (*id)()>
-        //   composition(f,g)(x) = f∘g(x) = f(g(x))
-        // aclと同じ、maspyさん記事と逆
+//   composition(f,g)(x) = f∘g(x) = f(g(x))
+// aclと同じ、maspyさん記事と逆
 struct lazy_segtree {
     vector<S> v;
     vector<F> vf;
@@ -27,12 +27,12 @@ struct lazy_segtree {
         propagate_above(r0);
         while(l < r) {
             if(l & 1) {
-                vf[l] = composition(f, vf[l]);
+                apply_at(l, f);
                 l++;
             }
             if(r & 1) {
                 r--;
-                vf[r] = composition(f, vf[r]);
+                apply_at(r, f);
             }
             l >>= 1;
             r >>= 1;
@@ -46,13 +46,12 @@ struct lazy_segtree {
         for(ll i = maxi; i > 0; i--) {
             propagate_at(x >> i);
         }
-        return mapping(vf[x], v[x]);
+        return v[x];
     }
     void set(ll x, S s) {
         x += n;
         propagate_above(x);
         v[x] = s;
-        vf[x] = id();
         recul_above(x);
     }
     S prod(ll l, ll r) {
@@ -65,12 +64,12 @@ struct lazy_segtree {
         S sl = e(), sr = e();
         while(l < r) {
             if(l & 1) {
-                sl = op(sl, mapping(vf[l], v[l]));
+                sl = op(sl, v[l]);
                 l++;
             }
             if(r & 1) {
                 r--;
-                sr = op(mapping(vf[r], v[r]), sr);
+                sr = op(v[r], sr);
             }
             l >>= 1;
             r >>= 1;
@@ -79,11 +78,14 @@ struct lazy_segtree {
     }
 
   private:
-    S eval_at(ll x) { return mapping(vf[x], v[x]); }
+    void apply_at(ll x, F f) {
+        v[x] = mapping(f, v[x]);
+        if(x < n)
+            vf[x] = composition(f, vf[x]);
+    }
     void propagate_at(ll x) {
-        v[x] = mapping(vf[x], v[x]);
-        vf[x << 1] = composition(vf[x], vf[x << 1]);
-        vf[x << 1 | 1] = composition(vf[x], vf[x << 1 | 1]);
+        apply_at(x << 1, vf[x]);
+        apply_at(x << 1 | 1, vf[x]);
         vf[x] = id();
     }
     ll bit_length(unsigned long long x) { return 64 - countl_zero(x); }
@@ -97,7 +99,7 @@ struct lazy_segtree {
     void recul_above(ll x) {
         while(x > 1) {
             x >>= 1;
-            v[x] = op(eval_at(x << 1), eval_at(x << 1 | 1));
+            v[x] = op(v[x << 1], v[x << 1 | 1]);
         }
     }
 };
