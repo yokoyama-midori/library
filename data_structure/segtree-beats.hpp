15b0f6c2aed50f5b257a4269a3d6e884c9e2e5a5
yokoyama-midori <nittundoraemon@gmail.com>
2024-12-13 18:40:17 +0900

    a

---
+++ data_structure/segtree-beats.hpp
@@ -0,0 +1,112 @@
+#include "template.hpp"
+// https://rsm9.hatenablog.com/entry/2021/02/01/220408
+
+template <class S, S (*op)(S, S), S (*e)(), class F, S (*mapping)(F, S),
+          F (*composition)(F, F), F (*id)()>
+//   composition(f,g)(x) = f∘g(x) = f(g(x))
+// aclと同じ、maspyさん記事と逆
+struct segtree_beats {
+    vector<S> v;
+    vector<F> vf;
+    ll n;
+    segtree_beats(ll n)
+        : n(n), v(vector<S>(2 * n, e())), vf(vector<F>(2 * n, id())) {};
+    segtree_beats(vector<S> v_) : n(v_.size()) {
+        vf = vector<F>(2 * n, id());
+        v = vector<S>(2 * n, e());
+        rep(i, n) { v[i + n] = v_[i]; }
+        for(ll i = n - 1; i > 0; i--) {
+            v[i] = op(v[i << 1], v[i << 1 | 1]);
+        }
+    }
+    void apply(ll l, ll r, F f) {
+        l += n;
+        r += n;
+        ll l0 = l / (l & -l);
+        ll r0 = r / (r & -r) - 1;
+        propagate_above(l0);
+        propagate_above(r0);
+        while(l < r) {
+            if(l & 1) {
+                apply_at(l, f);
+                l++;
+            }
+            if(r & 1) {
+                r--;
+                apply_at(r, f);
+            }
+            l >>= 1;
+            r >>= 1;
+        }
+        recul_above(l0);
+        recul_above(r0);
+    }
+    S get(ll x) {
+        x += n;
+        ll maxi = bit_length(x) - 1;
+        for(ll i = maxi; i > 0; i--) {
+            propagate_at(x >> i);
+        }
+        return v[x];
+    }
+    void set(ll x, S s) {
+        x += n;
+        propagate_above(x);
+        v[x] = s;
+        recul_above(x);
+    }
+    S prod(ll l, ll r) {
+        l += n;
+        r += n;
+        ll l0 = l / (l & -l);
+        ll r0 = r / (r & -r) - 1;
+        propagate_above(l0);
+        propagate_above(r0);
+        S sl = e(), sr = e();
+        while(l < r) {
+            if(l & 1) {
+                sl = op(sl, v[l]);
+                l++;
+            }
+            if(r & 1) {
+                r--;
+                sr = op(v[r], sr);
+            }
+            l >>= 1;
+            r >>= 1;
+        }
+        return op(sl, sr);
+    }
+
+  private:
+    void apply_at(ll x, F f) {
+        v[x] = mapping(f, v[x]);
+        if(x < n) {
+            vf[x] = composition(f, vf[x]);
+            // Added for Segment Tree Beats implementation.
+            if(v[x].fail) {
+                propagate_at(x);
+                v[x] = op(v[x << 1], v[x << 1 | 1]);
+            }
+        }
+    }
+    void propagate_at(ll x) {
+        apply_at(x << 1, vf[x]);
+        apply_at(x << 1 | 1, vf[x]);
+        vf[x] = id();
+    }
+    ll bit_length(unsigned long long x) { return 64 - countl_zero(x); }
+    void propagate_above(ll x) {
+        ll maxi = bit_length(x) - 1;
+        for(ll i = maxi; i > 0; i--) {
+            propagate_at(x >> i);
+        }
+        return;
+    }
+    void recul_above(ll x) {
+        while(x > 1) {
+            x >>= 1;
+            v[x] = op(v[x << 1], v[x << 1 | 1]);
+        }
+    }
+};
---
+++ test/yukicoder/880.test.cpp
@@ -0,0 +1,91 @@
+#define PROBLEM "https://yukicoder.me/problems/no/880"
+#include "data_structure/segtree-beats.hpp"
+#include "template.hpp"
+struct S {
+    ll mini, maxi, lcm, num, sum;
+    bool fail;
+    S(ll a, ll num_ = 1)
+        : mini(a), maxi(a), lcm(a), num(num_), sum(a * num_), fail(false) {};
+    S() = default;
+};
+S e() { return S(0, 0); }
+S op(S a, S b) {
+    S res = e();
+    res.mini = min(a.mini, b.mini);
+    res.maxi = max(a.maxi, b.maxi);
+    res.lcm = lcm(a.lcm, b.lcm);
+    if(a.lcm == inf or b.lcm == inf or
+       __int128_t(a.lcm) * b.lcm / (1 + gcd(a.lcm, b.lcm)) >= inf)
+        res.lcm = inf;
+    res.num = a.num + b.num;
+    res.sum = a.sum + b.sum;
+    res.fail = a.fail or b.fail;
+    return res;
+}
+struct F {
+    // gcdやってassign
+    bool gcd_q, assing_q;
+    ll g, x;
+};
+S mapping(F f, S s) {
+    if(s.fail) {
+        return s;
+    }
+    if(f.assing_q) {
+        return S(f.x, s.num);
+    }
+    if(f.gcd_q) {
+        if(s.mini == s.maxi) {
+            ll val = gcd(s.mini, f.g);
+            return S(val, s.num);
+        }
+        if(f.g % s.lcm)
+            s.fail = true;
+    }
+    return s;
+}
+F id() { return F(false, false, 0, 0); }
+F composition(F f1, F f2) {
+    if(f1.assing_q or (!f2.assing_q and !f2.gcd_q)) {
+        return f1;
+    }
+    if(!f1.gcd_q) {
+        return f2;
+    }
+    if(f2.assing_q) {
+        return F(false, true, 0, gcd(f1.g, f2.x));
+    }
+    f1.g = gcd(f1.g, f2.g);
+    return f1;
+}
+void solve() {
+    LL(n, q);
+    vector<S> a(n);
+    rep(i, n) {
+        LL(ai);
+        a[i] = S(ai);
+    }
+    segtree_beats<S, op, e, F, mapping, composition, id> seg(a);
+    while(q--) {
+        LL(flag);
+        LL(l, r);
+        l--;
+        if(flag == 1) {
+            LL(x);
+            seg.apply(l, r, F(false, true, 0, x));
+        } else if(flag == 2) {
+            LL(x);
+            seg.apply(l, r, F(true, false, x, 0));
+        } else if(flag == 3) {
+            print(seg.prod(l, r).maxi);
+        } else {
+            print(seg.prod(l, r).sum);
+        }
+    }
+}
+int main() {
+    ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    ll t = 1;
+    rep(_, t) solve();
+}
