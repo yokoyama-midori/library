5c541a9690c4dbf5071710236e4e1da8d095f0cf
yokoyama-midori <nittundoraemon@gmail.com>
2025-01-17 23:37:57 +0900

    a

---
+++ test/yukicoder/3004.test.cpp
@@ -0,0 +1,22 @@
+#include "math/factorial_large.hpp"
+#include <atcoder/modint>
+using mint = atcoder::modint998244353;
+factorial_large<mint> fac;
+void solve() {
+    LL(k);
+    ll sum = 0;
+    mint ans = 1;
+    rep(i, k) {
+        LL(l, m);
+        sum += l * m;
+        ans *= fac.fac(m);
+        ans *= fac.fac(l).pow(m);
+    }
+    ans = fac.fac(sum) / ans;
+    print(ans.val());
+}
+int main() {
+    ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    solve();
+}
