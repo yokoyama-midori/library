f7d60b589754b627f29771de92ae17274aada682
yokoyama-midori <nittundoraemon@gmail.com>
2025-02-10 20:25:34 +0900

    a

--- data_structure/sparse-table.hpp
+++ data_structure/sparse-table.hpp
@@ -3,10 +3,11 @@
 template <class T, auto op> struct SparseTable {
     SparseTable(const vector<T> &vec) {
         int n = vec.size();
-        int b = 32 - countl_zero(unsigned(n > 1 ? n - 1 : n));
-        v = vector(b, vector<T>(n));
+        int b = bit_width(unsigned(n));
+        v.resize(b);
         v[0] = vec;
         rep(i, b - 1) {
+            v[i + 1].resize(n - (1 << i));
             for(int j = 0; j + (1 << i) < n; j++) {
                 v[i + 1][j] = op(v[i][j], v[i][j + (1 << i)]);
             }
@@ -15,7 +16,7 @@ template <class T, auto op> struct SparseTable {
     T prod(int l, int r) const {
         if(l + 1 == r)
             return v[0][l];
-        int b = 31 - countl_zero(unsigned(r - l - 1));
+        int b = bit_width(unsigned(r - l - 1)) - 1;
         return op(v[b][l], v[b][r - (1 << b)]);
     }
 
