6671c3d6287e12a3d68300ceebdbcf929e388783
yokoyama-midori <nittundoraemon@gmail.com>
2025-03-05 07:27:57 +0900

    a

---
+++ math/linear-sieve.hpp
@@ -0,0 +1,42 @@
+#pragma once
+#include "template.hpp"
+// https://37zigen.com/linear-sieve/
+// AtCoder codetest
+// 1e6   7ms 7.5MB
+// 1e7  53ms  46MB
+// 1e8 650ms 426MB
+struct LinearSieve {
+    int max_value;
+    // lpf[i] = iの最小素因数(least prime factor)
+    vector<int> lpf, primes;
+    LinearSieve(int max_value) : max_value(max_value), lpf(max_value + 1, 1) {
+        for(int i = 2; i <= max_value; ++i) {
+            if(lpf[i] == 1) {
+                lpf[i] = i;
+                primes.emplace_back(i);
+            }
+            for(auto p : primes) {
+                if(p * i > max_value or p > lpf[i])
+                    break;
+                lpf[p * i] = p;
+            }
+        }
+    }
+    vector<int> factorize(int n) {
+        vector<int> res;
+        while(n > 1) {
+            res.emplace_back(lpf[n]);
+            n /= lpf[n];
+        }
+        return res;
+    }
+    vector<pair<int, int>> factor_count(int n) {
+        vector<pair<int, int>> res;
+        while(n > 1) {
+            if(res.empty() or res.back().first != lpf[n])
+                res.emplace_back(lpf[n], 0);
+            ++res.back().second;
+        }
+        return res;
+    }
+};
---
+++ test/yukicoder/2896.test.cpp
@@ -0,0 +1,23 @@
+#define PROBLEM "https://yukicoder.me/problems/no/2896"
+#include "math/factorial.hpp"
+#include "math/linear-sieve.hpp"
+#include <atcoder/modint>
+factorial<atcoder::modint998244353> fac;
+void solve() {
+    INT(q);
+    LinearSieve ls(1.1e5);
+    int num = 0;
+    rep(i, q) {
+        INT(a, b);
+        auto fa = ls.factorize(a);
+        assert(ranges::is_sorted(fa));
+        num += fa.size();
+        print(fac.binom(num - 1, b - 1).val());
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    solve();
+}
