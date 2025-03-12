5fe0672a1bf8d407bb3e381eb6a89a468d5fed0b
yokoyama-midori <nittundoraemon@gmail.com>
2025-03-12 15:02:33 +0900

    a

---
+++ string/wildcard-pattern-matching.hpp
@@ -0,0 +1,36 @@
+#pragma once
+#include "rng.hpp"
+#include "template.hpp"
+#include <atcoder/convolution>
+#include <atcoder/modint>
+template <class T>
+enable_if_t<is_integral_v<T>, vector<bool>>
+wildcard_pattern_matching(const vector<T> &_s, const vector<T> &_t) {
+    assert(_s.size() >= _t.size());
+    using mint = atcoder::modint998244353;
+    int ns = _s.size(), nt = _t.size();
+    vector<mint> s(ns), s01(ns), t(nt), t01(nt);
+    for(int i : views::iota(0, ns))
+        s[i] = _s[i], s01[i] = _s[i] ? 1 : 0;
+    for(int i : views::iota(0, nt)) {
+        mint r = rng(0, mint::mod());
+        t[i] = _t[nt - 1 - i] * r, t01[i] = _t[nt - 1 - i] ? r : 0;
+    }
+    auto c1 = atcoder::convolution(s, t01);
+    auto c2 = atcoder::convolution(s01, t);
+    vector<bool> res(ns - nt + 1);
+    for(int i : views::iota(0, ssize(res)))
+        res[i] = c1[nt - 1 + i] == c2[nt - 1 + i];
+    return res;
+}
+vector<bool> wildcard_pattern_matching(const string &str, const string &str2,
+                                       const char &wildcard = '*') {
+    vector<char> _s(begin(str), end(str)), _t(begin(str2), end(str2));
+    for(char &c : _s)
+        if(c == wildcard)
+            c = 0;
+    for(char &c : _t)
+        if(c == wildcard)
+            c = 0;
+    return wildcard_pattern_matching(_s, _t);
+}
---
+++ test/library_checker/string/wildcard_pattern_matching.test.cpp
@@ -0,0 +1,14 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/wildcard_pattern_matching"
+#include "string/wildcard-pattern-matching.hpp"
+void solve() {
+    STR(s, t);
+    auto ans = wildcard_pattern_matching(s, t);
+    for(auto i : ans)
+        cout << i;
+    print();
+}
+int main() {
+    ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    solve();
+}
