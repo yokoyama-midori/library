0fde8612901983f70c9f06d9e20a43886e888bbc
yokoyama-midori <nittundoraemon@gmail.com>
2025-03-15 14:57:47 +0900

    a

---
+++ test/yukicoder/2304.test.cpp
@@ -0,0 +1,22 @@
+#define PROBLEM "https://yukicoder.me/problems/no/2304"
+#include "data_structure/slope-trick.hpp"
+#include "template.hpp"
+void solve() {
+    INT(n);
+    vl a(n);
+    input(a);
+    ranges::sort(a);
+    SlopeTrick v;
+    for(auto ai : a) {
+        v.shift(1);
+        v.clear_right();
+        v.add_abs(ai);
+    }
+    print(v.min_f);
+}
+int main() {
+    ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    cout << std::setprecision(16);
+    solve();
+}
