e13f0ab23c91250ea14c5705bd293e93f847af24
yokoyama-midori <nittundoraemon@gmail.com>
2025-03-17 11:03:45 +0900

    test

---
+++ aplusb.test.cpp
@@ -0,0 +1,10 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/aplusb"
+#include <template.hpp>
+void solve() {
+    LL(a, b);
+    print(a + b);
+}
+int main() {
+    ll t = 1;
+    rep(_, t) solve();
+}
