7d3145afa6c4787172a2fbb8df06130e3bbdfb01
yokoyama-midori <nittundoraemon@gmail.com>
2025-04-06 14:43:57 +0900

    a

--- number-theory/kth-root-integer.hpp
+++ number-theory/kth-root-integer.hpp
@@ -3,7 +3,6 @@
  * @brief Kth Root (Integer)
  * @see https://ei1333.github.io/library/math/number-theory/kth-root-integer.hpp
  * @return floor(n^(1/k))
- * O(k log(n))時間
  * 浮動小数点数で計算してから調整する方法もあるが...
  */
 uint64_t kth_root_integer(uint64_t n, int k) {
@@ -22,7 +21,8 @@ uint64_t kth_root_integer(uint64_t n, int k) {
         return mul <= n;
     };
     uint64_t res = 0;
-    for(int i = 31; i >= 0; --i) {
+    // res <=  (2^64-1)^(1/3) < 2^(64/4) < 2^22
+    for(int i = 21; i >= 0; --i) {
         if(check(res | (1U << i)))
             res |= 1U << i;
     }
