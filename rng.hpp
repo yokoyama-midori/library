733f2d03ed3298c6fa635b035059115a785d2ba2
yokoyama-midori <nittundoraemon@gmail.com>
2025-04-29 13:00:48 +0900

    a

--- rng.hpp
+++ rng.hpp
@@ -1,6 +1,9 @@
 #pragma once
-// https://maspypy.github.io/library/random/base.hpp
 #include "template.hpp"
+/**
+ * @brief 乱数
+ * @see https://maspypy.github.io/library/random/base.hpp
+ */
 unsigned long long RNG_64() {
     static unsigned long long x_ =
         (unsigned long long)(chrono::duration_cast<chrono::nanoseconds>(
@@ -14,13 +17,15 @@ unsigned long long RNG_64() {
 
 unsigned long long RNG(unsigned long long lim) { return RNG_64() % lim; }
 
-ll rng(ll l, ll r) {
-    // [l,r)
-    return l + RNG_64() % (r - l);
-}
 /*
 1e18くらいの素数
 1000000000000000003
 1000000000000000009
 1000000000000000031
 */
+ll rng(ll l, ll r) {
+    // [l,r)
+    return l + RNG_64() % (r - l);
+}
+// [0,1]
+double rnd() { return RNG_64() * 5.42101086242752217e-20; }
