beba0007d5da604c9846713a069980b1c3f4890f
yokoyama-midori <nittundoraemon@gmail.com>
2025-04-29 11:16:34 +0900

    a

---
+++ other/timer.hpp
@@ -0,0 +1,16 @@
+#include "template.hpp"
+/**
+ * @brief タイマー
+ * @see https://ei1333.github.io/library/other/chrono-timer.hpp
+ * @see https://qiita.com/hurou927/items/a2d63837e731713c7a22
+ */
+struct Timer {
+    chrono::high_resolution_clock::time_point start;
+    Timer() { reset(); }
+    void reset() { start = chrono::high_resolution_clock::now(); }
+    // ミリ秒 型はint64_t?
+    chrono::milliseconds::rep operator()() const {
+        auto cur = chrono::high_resolution_clock::now();
+        return chrono::duration_cast<chrono::milliseconds>(cur - start).count();
+    }
+};
