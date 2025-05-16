fa801dff4c4551ce7caea1de749e9716e62be1ac
yokoyama-midori <nittundoraemon@gmail.com>
2025-05-16 15:06:37 +0900

    a

--- string/run-length-encoding.hpp
+++ string/run-length-encoding.hpp
@@ -1,10 +1,10 @@
 #pragma once
 #include "../template.hpp"
-template <class S>
-vector<pair<typename S::value_type, ll>> RunLengthEncoding(S &s) {
+template <class S, class count_type = ll>
+vector<pair<typename S::value_type, count_type>> RunLengthEncoding(const S &s) {
     using C = S::value_type;
-    vector<pair<C, ll>> res;
-    for(auto &&c : s) {
+    vector<pair<C, count_type>> res;
+    for(const C &c : s) {
         if(res.empty() or res.back().first != c) {
             res.emplace_back(c, 0);
         }
