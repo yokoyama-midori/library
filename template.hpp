130a734e6bec43a2b44ba79a775d17a770103f78
yokoyama-midori <nittundoraemon@gmail.com>
2025-05-20 15:22:42 +0900

    a

--- template.hpp
+++ template.hpp
@@ -77,11 +77,10 @@ template <class T> void input(vector<T> &a) {
     for(ll REP_COUNTER_##c = 0; REP_COUNTER_##c < (ll)(n); REP_COUNTER_##c++)
 #define REP1(n) REP1_0(n, __COUNTER__)
 #define REP2(i, a) for(ll i = 0; i < (ll)(a); i++)
-#define REP3(i, a, b) for(ll i = a; i < (ll)(b); i++)
-#define REP4(i, a, b, c) for(ll i = a; i < b; i += c)
+#define REP3(i, a, b) for(ll i = (ll)(a); i < (ll)(b); i++)
+#define REP4(i, a, b, c) for(ll i = (ll)(a); i < (ll)(b); i += (ll)(c))
 #define overload4(a, b, c, d, e, ...) e
 #define rep(...) overload4(__VA_ARGS__, REP4, REP3, REP2, REP1)(__VA_ARGS__)
-#define rep1(i, n) for(ll i = 1; i <= ((ll)n); ++i)
 ll inf = 3e18;
 vl dx = {1, -1, 0, 0};
 vl dy = {0, 0, 1, -1};
