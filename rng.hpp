7b0310f1f49fc8cf6776ec89f1a671c79121aba6
yokoyama-midori <nittundoraemon@gmail.com>
2025-06-17 14:06:56 +0900

    a

--- rng.hpp
+++ rng.hpp
@@ -1,18 +1,25 @@
 #pragma once
-#include "template.hpp"
+#include "./template.hpp"
 /**
  * @brief 乱数
  * @see https://maspypy.github.io/library/random/base.hpp
  */
+namespace rng_internal {
+unsigned long long x_ =
+    (unsigned long long)(chrono::duration_cast<chrono::nanoseconds>(
+                             chrono::high_resolution_clock::now()
+                                 .time_since_epoch())
+                             .count()) *
+    10150724397891781847ULL;
+}
+
 unsigned long long RNG_64() {
-    static unsigned long long x_ =
-        (unsigned long long)(chrono::duration_cast<chrono::nanoseconds>(
-                                 chrono::high_resolution_clock::now()
-                                     .time_since_epoch())
-                                 .count()) *
-        10150724397891781847ULL;
+    using namespace rng_internal;
     x_ ^= x_ << 7;
     return x_ ^= x_ >> 9;
+}
+void init_rng(unsigned long long seed = 10'000'000'000'000'000'000ULL) {
+    rng_internal::x_ = seed;
 }
 
 unsigned long long RNG(unsigned long long lim) { return RNG_64() % lim; }
@@ -24,8 +31,6 @@ unsigned long long RNG(unsigned long long lim) { return RNG_64() % lim; }
 1000000000000000031
 */
 // [l,r)
-ll rng(ll l, ll r) {
-    return l + RNG_64() % (r - l);
-}
+ll rng(ll l, ll r) { return l + RNG_64() % (r - l); }
 // [0,1]
 double rnd() { return RNG_64() * 5.42101086242752217e-20; }
