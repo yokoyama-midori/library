fbddb6abbd4d993a298870332129f55d69b6575c
yokoyama-midori <nittundoraemon@gmail.com>
2025-06-17 15:07:45 +0900

    test

--- rng.hpp
+++ rng.hpp
@@ -12,6 +12,7 @@ unsigned long long x_ =
                              .count()) *
     10150724397891781847ULL;
 }
+
 unsigned long long RNG_64() {
     using namespace rng_internal;
     x_ ^= x_ << 7;
