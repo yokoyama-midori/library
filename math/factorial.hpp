144b20cee776e0acce176bb1c8d9acefb72a26f9
yokoyama-midori <nittundoraemon@gmail.com>
2025-08-27 09:27:22 +0900

    a

--- math/factorial.hpp
+++ math/factorial.hpp
@@ -1,9 +1,11 @@
 #pragma once
 #include "../template.hpp"
-// https://suisen-cp.github.io/cp-library-cpp/library/math/factorial.hpp
+// 参考 https://suisen-cp.github.io/cp-library-cpp/library/math/factorial.hpp
 template <class T> struct factorial {
     factorial() {};
     void ensure(int n) {
+        auto &fac = get_fac();
+        auto &fac_inv = get_fac_inv();
         int sz = size(fac);
         if(sz > n) {
             return;
@@ -20,27 +22,42 @@ template <class T> struct factorial {
     }
     T operator[](int i) {
         ensure(i);
-        return fac[i];
+        return get_fac()[i];
     }
     T inv(int i) {
         ensure(i);
-        return fac_inv[i];
+        return get_fac_inv()[i];
     }
     T binom(int n, int i) {
         if(n < 0 or i < 0 or n < i) {
             return T(0);
         }
         ensure(n);
-        return fac[n] * fac_inv[i] * fac_inv[n - i];
+        return get_fac()[n] * get_fac_inv()[i] * get_fac_inv[n - i];
     }
     T perm(int n, int i) {
         if(n < 0 or i < 0 or n < i) {
             return T(0);
         }
         ensure(n);
-        return fac[n] * fac_inv[n - i];
+        return get_fac()[n] * get_fac_inv()[n - i];
     }
 
   private:
-    inline static vector<T> fac{1}, fac_inv{1};
+    /*
+     inline static vector<T> fac{1};
+     のように書くと static initialization order fiasco と呼ばれる問題が起きる
+     この問題を避けるため、関数内の static 変数として定義するイディオム
+     (Construct on First Use / Meyers' Singleton) を用いる
+
+     https://in-neuro.hatenablog.com/entry/2020/12/12/001244
+    */
+    static auto &get_fac() {
+        static vector<T> fac_{1};
+        return fac_;
+    }
+    static auto &get_fac_inv() {
+        static vector<T> fac_inv_{1};
+        return fac_inv_;
+    }
 };
