1c74149720fd28c28c0a9b906932424bec8f0f9d
yokoyama-midori <nittundoraemon@gmail.com>
2025-09-07 10:01:21 +0900

    a

--- data-structure/compress.hpp
+++ data-structure/compress.hpp
@@ -5,25 +5,24 @@ template <class T> struct Compress {
     bool is_built = false;
     vector<T> data;
     Compress() = default;
-    Compress(const vector<T> &v) { add(v); }
+    explicit Compress(const vector<T> &v) { add(v); }
     void add(const T &x) {
         is_built = false;
         data.emplace_back(x);
     }
     void add(const vector<T> &v) {
-        for(auto x : v)
-            add(x);
+        is_built = false;
+        ranges::copy(v, back_inserter(data));
     }
     void build() {
         is_built = true;
-        sort(data.begin(), data.end());
-        data.erase(unique(data.begin(), data.end()), data.end());
+        ranges::sort(data);
+        data.erase(unique(begin(data), end(data)), end(data));
     }
+    // x 以上となる最小のインデックスを返す
     int get(const T &x) const {
-        // x 以上となる最小のインデックスを返す
         assert(is_built);
-        int res = lower_bound(data.begin(), data.end(), x) - data.begin();
-        return res;
+        return ranges::lower_bound(data, x) - begin(data);
     }
     const T &operator[](int t) {
         assert(is_built);
